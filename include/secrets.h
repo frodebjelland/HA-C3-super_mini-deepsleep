c71b3cacce47eaf04aee21b1341d77614e7c1bb6
frodebjelland <149882490+frodebjelland@users.noreply.github.com>
2025-07-18 09:02:20 +0200

    Update secrets.h
    Entered example variables and values

--- include/secrets.h
+++ include/secrets.h
@@ -1,1 +1,12 @@
+// secrets.h
 
+#pragma once
+
+// ——— Wi‑Fi credentials ———————————————————————————————
+#define MY_SECRET_SSID     "Your_WIFI_SSID"
+#define MY_SECRET_PASS     "Your_WIFI_Password"
+
+// ——— Static IP configuration —————————————————————————
+#define MY_SECRET_STATIC_IP   "192.168.1.50"
+#define MY_SECRET_STATIC_MASK "255.255.255.0"
+#define MY_SECRET_STATIC_GW   "192.168.1.1"
